684c966331aeb8976e03b5517076bf43c01e6a87
navigatrum <6989338+navigatrum@users.noreply.github.com>
2022-07-14 23:07:25 +0200

    add `validateTimeProps` function

--- packages/siwe-parser/lib/utils.ts
+++ packages/siwe-parser/lib/utils.ts
@@ -18,3 +18,19 @@ export const isEIP55Address = (address: string) => {
     }
     return address === ret;
 }
+
+/**
+ * Validates `issuedAt`, `expirationTime` and `notBefore` object props against ISO-8601 time.
+ * @param obj Abnf `ParsedMessage` or `SiweMessage` to be checked.
+ * @param errorMsg The error message to be thrown when validation fails.
+ */
+export const validateTimeProps = (obj: any, errorMsg = 'Invalid time format.') => {
+    const ISO8601 =
+        /([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(\.[0-9]+)?(([Zz])|([+|\-]([01][0-9]|2[0-3]):[0-5][0-9]))/;
+
+    ['issuedAt', 'expirationTime', 'notBefore'].forEach(prop => {
+        if (prop in obj && !ISO8601.test(obj.prop)) {
+            throw new Error(errorMsg);
+        }
+    });
+}
